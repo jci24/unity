b1d0a85e795a222255377bfd9654f3104a4e5f50
jci24 <77500633+jci24@users.noreply.github.com>
2022-03-07 16:52:39 +0100

    Update FollowUpQuestion.cs

--- FollowUpQuestion.cs
+++ FollowUpQuestion.cs
@@ -10,15 +10,13 @@ public class VolumeTest
     // Start is called before the first frame update
     void Start()
     {
-        var audiosource = new VolumeProperty();
-        var volume = 0;
+        var audiosource = GetComponent<AudioSource>();
         var expectedVolume = -6;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var volume = VolumeProperty.volume
-        Assert.That(volume, IS.EqualTo(expectedVolume));
+        Assert.That(audiosource.volume, IS.EqualTo(expectedVolume));
     }
 }
