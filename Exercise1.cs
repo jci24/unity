0286f3a010102229350b8a354cc300b0fbc710b9
jci24 <77500633+jci24@users.noreply.github.com>
2022-03-07 16:40:39 +0100

    Update Exercise1.cs

--- Exercise1.cs
+++ Exercise1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NUnit.Framework;
-
+using UnityEngine.TestTools; 
 public class VolumeTarget 
 {
     [Test]
@@ -13,7 +13,7 @@ public class VolumeTarget
         var volume = 0;
         var expectedVolume = -6;
         //ACT
-        var volume = AudioMixer.SetFloat//Function that returns the value of the volume of the snapshot
+        var volume = AudioMixer.GetFloat() //Function that returns the value of the volume of the snapshot
         //ASSERT
          Assert.That(volume, IS.EqualTo(expectedVolume));
     }
